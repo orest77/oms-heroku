25893978dc8454f03dc867f2a10d67e70caac851
Orest <orest@localhost.localdomain>
2018-04-06 01:14:28 +0300

    ADD new sql conector77

--- src/main/java/com/softserveinc/edu/oms/persistence/dao/HibernateDao.java
+++ src/main/java/com/softserveinc/edu/oms/persistence/dao/HibernateDao.java
@@ -204,13 +204,3 @@ public class HibernateDao<T extends AbstractEntity> implements IDao<T> {
 		this.sessionFactory = sessionFactory;
 	}
 }
-Map<String,String> jdbcUrlSettings = new HashMap<>();
-String jdbcDbUrl = System.getenv("JDBC_DATABASE_URL");
-if (null != jdbcDbUrl) {
-  jdbcUrlSettings.put("hibernate.connection.url", System.getenv("JDBC_DATABASE_URL"));
-}
-
-registry = new StandardServiceRegistryBuilder().
-    configure("hibernate.cfg.xml").
-    applySettings(jdbcUrlSettings).
-    build();
